b66ea27ade9246a8adea088c337817693c1b378c
nameisjayant <jayntkumar99@gmail.com>
2023-01-29 12:20:01 +0530

    food screen added

--- app/src/main/java/com/programmingsimplified/cinemabookingapp/screens/food_screen.kt
+++ app/src/main/java/com/programmingsimplified/cinemabookingapp/screens/food_screen.kt
@@ -1,16 +1,143 @@
 package com.programmingsimplified.cinemabookingapp.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import com.programmingsimplified.cinemabookingapp.ui.theme.GradientBackground
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.programmingsimplified.cinemabookingapp.R
+import com.programmingsimplified.cinemabookingapp.commonUi.SpacerHeight
+import com.programmingsimplified.cinemabookingapp.ui.theme.*
 
 
 @Composable
 fun FoodScreen() {
 
-    Box(modifier = Modifier.fillMaxSize().background(GradientBackground))
+    var search by remember { mutableStateOf("") }
 
+
+    Box(
+        modifier = Modifier.padding(bottom = 60.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DarkBlack)
+        ) {
+            Column {
+                FoodHeader()
+                FoodMenu(text = search, onValueChange = { search = it })
+            }
+        }
+    }
+}
+
+@Composable
+fun FoodMenu(
+    text: String,
+    onValueChange: (String) -> Unit
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .padding(vertical = 20.dp, horizontal = 30.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.menu),
+            style = TextStyle(
+                fontSize = 27.sp,
+                fontWeight = FontWeight.W800,
+                fontFamily = InterFont,
+                color = Color.White
+            ),
+        )
+        SpacerHeight(20.dp)
+        CustomSearchView(text = text, onValueChange = { onValueChange(it) })
+    }
+
+}
+
+@Composable
+fun CustomSearchView(
+    text: String,
+    onValueChange: (String) -> Unit
+) {
+    Box(modifier = Modifier.clip(RoundedCornerShape(10.dp))) {
+        TextField(
+            value = text, onValueChange = {
+                onValueChange(it)
+            },
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = LightGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                placeholderColor = DarkGray,
+                textColor = Color.White
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.search),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.W400,
+                        fontFamily = InterFont
+                    )
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.search), contentDescription = "",
+                    tint = Color.White
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.microphone), contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun FoodHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box {}
+        Text(
+            text = stringResource(R.string.FOOD), style = TextStyle(
+                fontSize = 27.sp,
+                fontWeight = FontWeight.W800,
+                fontFamily = InterFont,
+                color = Color.White
+            ),
+            modifier = Modifier.align(CenterVertically)
+        )
+        IconButton(onClick = {}, modifier = Modifier.size(48.dp)) {
+            Icon(
+                painter = painterResource(id = com.programmingsimplified.cinemabookingapp.R.drawable.notification),
+                contentDescription = "",
+                tint = Color.Unspecified
+            )
+        }
+    }
 }
--- app/src/main/java/com/programmingsimplified/cinemabookingapp/ui/theme/Color.kt
+++ app/src/main/java/com/programmingsimplified/cinemabookingapp/ui/theme/Color.kt
@@ -9,10 +9,11 @@ val Purple700 = Color(0XFF151520)
 val Teal200 = Color(0XFF151520)
 
 val DarkBlack = Color(0XFF151520)
-val LightBlack =  Color(0XFF262630)
+val LightBlack = Color(0XFF262630)
 val LightGray = Color(0XFF2B2B38)
 val DarkRed = Color(0XFFF74346)
 val GrayColor = Color(0XFF4A4B56)
+val DarkGray = Color(0XFF4A4B56)
 
 val GradientBackground = Brush.verticalGradient(
     listOf(
